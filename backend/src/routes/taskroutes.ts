bf674071fe1fb68ee8a7c3349d900f37d664958c
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-04 13:26:36 +0200

    feat(routes) : adding tasks routes

---
+++ backend/src/routes/taskroutes.ts
@@ -0,0 +1,25 @@
+import { taskStore } from "../models/taskstore";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/tasks", (req, res) => {
+    res.json(taskStore.getAll());
+});
+
+router.post("/tasks", (req, res) => {
+    const task = taskStore.add(req.body);
+    res.json(task);
+});
+
+router.put("tasks/:id", (req, res) => {
+    const task = taskStore.updateStatus(req.params.id, req.body.status);
+    res.json(task);
+});
+
+router.delete("/tasks/:id", (req, res) => {
+    const result = taskStore.delete(req.params.id);
+    res.json(result);
+});
+
+export default router;
