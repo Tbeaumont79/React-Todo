361ddf19a794b709554f9fca20909b339702629b
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-14 16:03:58 +0200

    feat: update the style of the task form

--- frontend/src/component/taskForm.tsx
+++ frontend/src/component/taskForm.tsx
@@ -16,24 +16,31 @@ export const TodoForm = ({ onSubmit }: Props) => {
 
 	return (
 		<form
-			className="border-1 w-[600px] h-96 border-purple-50 bg-purple-50 text-white flex flex-col gap-y-5 p-10"
+			className="w-[600px] h-96 bg-gray-300 text-white flex flex-col gap-y-10 p-10 rounded-2xl"
 			onSubmit={handleSubmit((data) => {
 				onSubmit(data);
 				reset();
 			})}
 		>
+			{errors.title && (
+				<p className="text-red-600 bg-red-300 p-2 rounded-2xl">
+					{errors.title.message}
+				</p>
+			)}
 			<input
 				{...register("title")}
 				placeholder="Titre"
-				className="placeholder-white bg-black rounded-2xl py-2 px-5"
+				className="placeholder-white bg-gray-800 rounded-2xl py-3 px-5 shadow-xs shadow-gray-950"
 			/>
-			{errors.title && <p className="text-red-500">{errors.title.message}</p>}
 			<textarea
 				{...register("description")}
 				placeholder="Description"
-				className="placeholder-white bg-black rounded-2xl p-5"
+				className="h-96 min-h-10 placeholder-white bg-gray-800 rounded-2xl py-2 px-5 shadow-xs shadow-gray-950 resize-none"
 			/>
-			<button type="submit" className="bg-black py-2 px-10 rounded-xl w-40">
+			<button
+				type="submit"
+				className="bg-orange-600 py-3 px-10 rounded-xl w-40 hover:bg-orange-400 shadow-xs shadow-amber-900"
+			>
 				Ajouter
 			</button>
 		</form>
