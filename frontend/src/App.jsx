8dc76e68d1bd17cd7d4cf83f89ee0a1fe099c032
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-14 15:42:59 +0200

    feat: adding the taskform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { TodoForm } from "./component/taskForm";
 function App() {
 	return (
-		<main className="bg-blue-950">
+		<main className="bg-blue-950 h-screen flex justify-center items-center">
 			<TodoForm />
 		</main>
 	);
--- frontend/src/component/taskForm.tsx
+++ frontend/src/component/taskForm.tsx
@@ -16,15 +16,26 @@ export const TodoForm = ({ onSubmit }: Props) => {
 
 	return (
 		<form
+			className="border-1 w-[600px] h-96 border-purple-50 bg-purple-50 text-white flex flex-col gap-y-5 p-10"
 			onSubmit={handleSubmit((data) => {
 				onSubmit(data);
 				reset();
 			})}
 		>
-			<input {...register("title")} placeholder="Titre" />
-			{errors.title && <p>{errors.title.message}</p>}
-			<textarea {...register("description")} placeholder="Description" />
-			<button type="submit">Ajouter</button>
+			<input
+				{...register("title")}
+				placeholder="Titre"
+				className="placeholder-white bg-black rounded-2xl py-2 px-5"
+			/>
+			{errors.title && <p className="text-red-500">{errors.title.message}</p>}
+			<textarea
+				{...register("description")}
+				placeholder="Description"
+				className="placeholder-white bg-black rounded-2xl p-5"
+			/>
+			<button type="submit" className="bg-black py-2 px-10 rounded-xl w-40">
+				Ajouter
+			</button>
 		</form>
 	);
 };
