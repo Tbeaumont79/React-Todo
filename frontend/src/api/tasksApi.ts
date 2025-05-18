62fd52437cfb1ef118935c6da0fe94faeeac901b
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-18 10:02:37 +0200

    feat: adding a new tasksapi that handle post, get and delete

---
+++ frontend/src/api/tasksApi.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { TodoInput } from "../schema/todoSchema";
+
+const API_URL = "http://localhost:3000/api/tasks";
+
+export const getAllTasks = async () => {
+	const data = await axios.get(API_URL);
+	return data;
+};
+
+export const createTask = async (task: TodoInput) => {
+	const { data } = await axios.post(API_URL, task);
+	return data;
+};
+
+export const deleteTask = async (id: number) => {
+	const { data } = await axios.delete(`${API_URL}/${id}`);
+	return data;
+};
