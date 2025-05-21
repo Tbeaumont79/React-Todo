d9f97f3cc4cb3f196fbd43d82402ed56b272e54d
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-21 17:03:56 +0200

    refacto: creating a taskList.tsx component

--- frontend/src/component/taskApp.tsx
+++ frontend/src/component/taskApp.tsx
@@ -1,56 +1,14 @@
 import { TodoForm } from "./taskForm";
+import { TaskList } from "./taskList";
 import { useTasks } from "../hooks/useTasks";
-import Task from "../types/Task";
 
 export function TaskApp() {
-	const { data: tasksResponse, isLoading, createTask, deleteTask } = useTasks();
-
-	const tasks = tasksResponse && tasksResponse.data;
+	const { isLoading, createTask } = useTasks();
 	return (
 		<div className="h-full w-full flex flex-col justify-center items-center gap-y-10">
 			<h1 className="text-3xl pt-5">Mes tâches</h1>
 			<TodoForm onSubmit={createTask} />
-			{isLoading ? (
-				<p>Chargement ...</p>
-			) : (
-				tasks && (
-					<div>
-						<table className="min-w-full border border-gray-300">
-							<thead>
-								<tr>
-									<th className="px-4 py-2 border-b">Titre</th>
-									<th className="px-4 py-2 border-b">Description</th>
-									<th className="px-4 py-2 border-b">Statut</th>
-									<th className="px-4 py-2 border-b">Actions</th>
-								</tr>
-							</thead>
-							<tbody>
-								{tasks.map((task: Task) => (
-									<tr key={task.id} className="text-center">
-										<td className="px-4 py-2 border-b font-bold">
-											{task.title}
-										</td>
-										<td className="px-4 py-2 border-b">{task.description}</td>
-										<td className="px-4 py-2 border-b">
-											<span className="bg-blue-500 text-white rounded-xl px-2 py-1">
-												{task.status}
-											</span>
-										</td>
-										<td className="px-4 py-2 border-b">
-											<button
-												className="bg-red-500 text-white px-3 py-1 rounded-xl hover:bg-red-600"
-												onClick={() => deleteTask(String(task.id))}
-											>
-												Supprimer
-											</button>
-										</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-					</div>
-				)
-			)}
+			{isLoading ? <p>Chargement ...</p> : <TaskList />}
 		</div>
 	);
 }
---
+++ frontend/src/component/taskList.tsx
@@ -0,0 +1,43 @@
+import Task from "../types/Task";
+import { useTasks } from "../hooks/useTasks";
+
+export function TaskList() {
+	const { data: tasksResponse, deleteTask } = useTasks();
+
+	const tasks = tasksResponse && tasksResponse.data;
+	return (
+		<div>
+			<table className="min-w-full border border-gray-300">
+				<thead>
+					<tr>
+						<th className="px-4 py-2 border-b">Titre</th>
+						<th className="px-4 py-2 border-b">Description</th>
+						<th className="px-4 py-2 border-b">Statut</th>
+						<th className="px-4 py-2 border-b">Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					{tasks.map((task: Task) => (
+						<tr key={task.id} className="text-center">
+							<td className="px-4 py-2 border-b font-bold">{task.title}</td>
+							<td className="px-4 py-2 border-b">{task.description}</td>
+							<td className="px-4 py-2 border-b">
+								<span className="bg-blue-500 text-white rounded-xl px-2 py-1">
+									{task.status}
+								</span>
+							</td>
+							<td className="px-4 py-2 border-b">
+								<button
+									className="bg-red-500 text-white px-3 py-1 rounded-xl hover:bg-red-600"
+									onClick={() => deleteTask(String(task.id))}
+								>
+									Supprimer
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+}
